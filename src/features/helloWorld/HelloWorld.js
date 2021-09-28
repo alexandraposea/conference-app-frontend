40dca77e2854753b832b22f638dad4a09c432492
alexandraposea98@yahoo.com <alexandraposea98@yahoo.com>
2021-09-28 16:01:18 +0300

    add helloWorld

--- src/features/helloWorld/HelloWorld.js
+++ src/features/helloWorld/HelloWorld.js
@@ -1,7 +1,17 @@
+import React from 'react'
+import { useQuery } from "@apollo/client";
+import {MY_FIRST_QUERY} from './queries/MyFirstQuery'
+import { LoadingFakeText } from '@bit/totalsoft_oss.react-mui.kit.core';
+
 
 function HelloWorld() {
+    const {loading, data} = useQuery(MY_FIRST_QUERY);
+    
+    if(loading){
+        return <LoadingFakeText lines={10}/>
+    }
 
-    return "Hello World! 🐱‍👤"
+    return data?.myFirstEndpoint
 }
 
 export default HelloWorld;
